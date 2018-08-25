bebd009f71c9df6eb16b42d38d8963b461402b83
Joseph Salas Solano <josephsalas1112@gmail.com>
2018-08-24 18:47:48 -0600

    Prueba2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hola Mundo");
+        primaryStage.setTitle("Hola prueba mundo");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
