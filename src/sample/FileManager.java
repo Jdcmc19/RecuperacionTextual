d778aa4c8c964432df16275a07acb6f196c4f5fc
Jdcmc19 <35380586+Jdcmc19@users.noreply.github.com>
2018-08-24 23:57:05 -0600

    Diccionario

--- src/sample/FileManager.java
+++ src/sample/FileManager.java
@@ -1,9 +1,9 @@
 package sample;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class FileManager {
 
@@ -38,6 +38,62 @@ public class FileManager {
                     System.out.println(file.getName());
                 }
             }
+        }
+    }
+    public void createMap() throws FileNotFoundException {
+        File file = new File("C:\\Users\\iworth\\iCloudDrive\\Documents\\TEC\\Semestre II 2018\\Recuperacion textual\\Proyecto\\Tarea programada 1\\man-es\\man2\\accept.2");
+        Scanner sc = new Scanner(file);
+
+        sc.useDelimiter("\\Z");
+        String texto = sc.next();
+        String[] lineas = texto.split("\n");
+        texto = "";
+        for(String linea: lineas){
+            linea = linea.trim();
+            String[] words = linea.split(" ");
+            if(words.length>0 && !words[0].equals(".\\\"")) {
+                texto+=linea+" ";
+            }
+        }
+        texto = texto.substring(0,texto.length()-1).toLowerCase();
+        String[] palabras;
+        texto = texto.replace("\t"," ").replace("á","a")
+                .replace("é","e").replace("í","i")
+                .replace("ó","o").replace("ú","u")
+                .replace("."," ").replace(","," ")
+                .replace("_"," ").replaceAll("\\W"," ").replace("-"," ");
+               /* .replace("]"," ").replace("["," ")
+                .replace("}"," ").replace("{"," ")
+                .replace(")"," ").replace(")"," ");*/
+        palabras = texto.split(" ");
+
+        String[] stop = {"a","ante","bajo","cabe","con","contra","de","desde","e","el","en","entre","hacia","hasta","ni","la","le","lo","los","las","o","para","pero","por","que","segun","sin","so","uno","unas","unos","y","sobre","tras","u","un","una"};
+        ArrayList<String> stopWords = new ArrayList<>(Arrays.asList(stop));
+        ArrayList<String> terminos = new ArrayList<>(Arrays.asList(palabras));
+        for(String palabra : stopWords){
+            while(terminos.remove(palabra));
+        }
+
+        String a = "";
+        Map<String,Integer> diccionario = new HashMap<String,Integer>();
+        for(int e=0;e<terminos.size();e++){
+            String tmp = terminos.get(e);/*
+            if(tmp.length()>0 && (tmp.charAt(0)=='(' || tmp.charAt(0)=='[' || tmp.charAt(0)=='{' || tmp.charAt(0)=='\"' ))
+                tmp=tmp.substring(1);
+            if(tmp.length()>0 && (tmp.charAt(tmp.length()-1)==')' || tmp.charAt(tmp.length()-1)==']' || tmp.charAt(tmp.length()-1)=='}' || tmp.charAt(tmp.length()-1)=='\"' ))
+                tmp=tmp.substring(0,tmp.length()-1);*/
+            if(!tmp.equals("")){
+                if(diccionario.containsKey(tmp)){
+                    diccionario.put(tmp,diccionario.get(tmp)+1);
+                }else{
+                    diccionario.put(tmp,1);
+                }
+            }
+        }
+        ArrayList<String> llaves = new ArrayList<>(diccionario.keySet());
+        Collections.sort(llaves, String.CASE_INSENSITIVE_ORDER);
+        for(String termino : llaves){
+            System.out.println(termino+": "+diccionario.get(termino));
         }
     }
 }
